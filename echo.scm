c9742dba2d43e7df35fad434d80400e8c4b4962d
Chris K. Jester-Young <cky944@gmail.com>
2013-03-13 18:21:42 -0400

    Use a string-port variant rather than regex.
    This approach is harder to read, but it's faster and more importantly,
    it doesn't involve compiled regex literals, which Guile's compiler
    chokes on.

--- echo.scm
+++ echo.scm
@@ -1,9 +1,8 @@
 #!/usr/bin/guile \
---no-auto-compile -e main -s
+-e main -s
 !#
 
-(use-modules (ice-9 binary-ports)
-             (ice-9 regex))
+(use-modules (ice-9 binary-ports))
 
 (define status #t)
 
@@ -17,55 +16,51 @@
            (unless first
              (write-char #\space))
            (let ((arg (car args)))
-             (echo arg)
+             (call-with-input-string arg echo)
              (loop (cdr args) #f))))))
 
-(define-syntax regexp-case
-  (lambda (stx)
-    (syntax-case stx (=> else)
-      ((_ (key ...) pos clauses ...)
-       #'(let ((atom-key (key ...)))
-           (regexp-case atom-key pos clauses ...)))
-      ((_ key pos (else expr ...))
-       #'(begin
-           expr ...
-           pos))
-      ((_ key pos (regexp => func) next ...)
-       (with-syntax ((compiled (make-regexp (syntax->datum #'regexp))))
-         #'(let ((match (regexp-exec compiled key pos)))
-             (if match
-                 (begin
-                   (func (match:substring match))
-                   (match:end match))
-                 (regexp-case key pos next ...)))))
-      ((_ key pos (regexp expr ...) next ...)
-       #'(regexp-case key pos (regexp => (lambda _ expr ...)) next ...)))))
+(define (echo port)
+  (define ch (read-char port))
+  (cond ((eqv? ch #\\)
+         (backslash port))
+        ((not (eof-object? ch))
+         (write-char ch)
+         (echo port))))
 
-(define (echo str)
-  (define len (string-length str))
-  (let loop ((pos 0))
-    (when (< pos len)
-      (let ((ch (string-ref str pos)))
-        (cond ((eqv? ch #\\)
-               (loop (regexp-case str (1+ pos)
-                       ("^a" (write-char #\alarm))
-                       ("^b" (write-char #\backspace))
-                       ("^c" (quit status))
-                       ("^f" (write-char #\page))
-                       ("^n" (write-char #\newline))
-                       ("^r" (write-char #\return))
-                       ("^t" (write-char #\tab))
-                       ("^v" (write-char #\vtab))
-                       ("^\\\\" (write-char #\\))
-                       ("^0[1-7][0-7]{0,2}"
-                        => (lambda (substr)
-                             (define val (string->number substr 8))
-                             (if (< val 256)
-                                 (put-u8 (current-output-port) val)
-                                 (set! status #f))))
-                       ("^0" (write-char #\nul))
-                       (else (set! status #f)
-                             (write-char #\\)))))
-              (else
-               (write-char ch)
-               (loop (1+ pos))))))))
+(define (backslash port)
+  (define ch (read-char port))
+  (case ch
+    ((#\a #\b #\f #\n #\r #\t #\v #\\)
+     (display (call-with-input-string (string #\" #\\ ch #\") read))
+     (echo port))
+    ((#\c) (quit status))
+    ((#\0) (let ((next (peek-char port)))
+             (if (and (char? next) (char<=? #\1 next #\7))
+                 (octal port)
+                 (begin
+                   (write-char #\nul)
+                   (echo port)))))
+    (else (set! status #f)
+          (write-char #\\)
+          (unless (eof-object? ch)
+            (unread-char ch port)
+            (echo port)))))
+
+(define (octal port)
+  (let loop ((value 0)
+             (waiting 3))
+    (cond ((zero? waiting)
+           (if (< value 256)
+               (put-u8 (current-output-port) value)
+               (set! status #f))
+           (echo port))
+          (else (let ((next (read-char port)))
+                  (cond ((eof-object? next)
+                         (loop value 0))
+                        ((char<=? #\0 next #\7)
+                         (loop (+ (* value 8) (- (char->integer next)
+                                                 (char->integer #\0)))
+                               (1- waiting)))
+                        (else
+                         (unread-char next port)
+                         (loop value 0))))))))
